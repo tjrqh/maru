fc753069f6538b46b6fd104ea1daccf1bd2b4f2e
tjrqh <asdww6631@gmail.com>
2024-07-09 16:11:37 +0900

    repector: userId값 파싱해서 저장

--- src/main/java/project/maru/application/dto/VoiceRecordsDto/VoiceRecordsCreateRequest.java
+++ src/main/java/project/maru/application/dto/VoiceRecordsDto/VoiceRecordsCreateRequest.java
@@ -4,15 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class VoiceRecordsCreateRequest {
 
   private String userId;
   private int questionsKrId;
   private String speechToText;
   private int matchingRate;
+
 }
--- src/main/java/project/maru/presentation/VoiceRecordsController.java
+++ src/main/java/project/maru/presentation/VoiceRecordsController.java
@@ -1,13 +1,16 @@
 package project.maru.presentation;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.maru.application.dto.VoiceRecordsDto.VoiceRecordsCreateRequest;
 import project.maru.application.service.VoiceRecordsService;
 import project.maru.domain.VoiceRecords;
+import project.maru.presentation.util.CognitoJWTParser;
 
 @RestController
 @RequestMapping("/voice")
@@ -16,10 +19,18 @@ public class VoiceRecordsController {
   @Autowired
   private VoiceRecordsService voiceRecordsService;
 
+  @Autowired
+  private CognitoJWTParser cognitoJWTParser;
+
 
   @PostMapping("/uploads")
-  public VoiceRecords postVoiceRecords(
-      @RequestParam VoiceRecordsCreateRequest voiceRecordsCreateRequest) {
+  public VoiceRecords postVoiceRecords(@RequestHeader("Authorization") String accessToken,
+      @RequestBody VoiceRecordsCreateRequest voiceRecordsCreateRequest) throws Exception {
+    DecodedJWT cognitoToken = cognitoJWTParser.parseToken(accessToken);
+    String sub = cognitoToken.getClaims().get("sub").toString();
+    String userId = sub.replaceAll("^\"|\"$", ""); // 앞뒤 큰따옴표 제거
+    voiceRecordsCreateRequest.setUserId(userId);
+    System.out.println(voiceRecordsCreateRequest);
     return voiceRecordsService.postVoiceRecords(voiceRecordsCreateRequest);
   }
 }
