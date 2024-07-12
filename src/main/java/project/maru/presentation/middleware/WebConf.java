30ffa7c8a0ca0f6805a38960b5475f51b62aa2bc
bm150 <jbm20dev@gmail.com>
2024-07-12 11:01:22 +0900

    feat : cors 설정

---
+++ src/main/java/project/maru/presentation/middleware/WebConf.java
@@ -0,0 +1,23 @@
+package project.maru.presentation.middleware;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConf implements WebMvcConfigurer {
+  @Value("${cors-allow-domain.front-end}")
+  private String ALLOW_DOMAIN;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins(ALLOW_DOMAIN,"http://localhost:3000")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("Authorization", "Content-Type")
+        .allowCredentials(true)
+        .maxAge(3600);
+    WebMvcConfigurer.super.addCorsMappings(registry);
+  }
+}
