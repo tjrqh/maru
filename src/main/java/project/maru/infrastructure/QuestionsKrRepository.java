b2c8b987bfb81837523b3602c6d07c804ad8bc46
bm150 <jbm20dev@gmail.com>
2024-07-13 19:44:29 +0900

    #64 feat : 전체 문제 수(kr테이블) 조회 기능 구현

--- src/main/java/project/maru/infrastructure/QuestionsKrRepository.java
+++ src/main/java/project/maru/infrastructure/QuestionsKrRepository.java
@@ -1,6 +1,7 @@
 package project.maru.instructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import project.maru.application.dto.questionKrDto.QuestionsKrReadResponse;
 import project.maru.domain.QuestionsKr;
 
@@ -11,5 +12,9 @@ public interface QuestionsKrRepository extends JpaRepository<QuestionsKr, String
   QuestionsKrReadResponse findByContentTypeId(int contentTypeId);
 
   QuestionsKr findById(int id);
+
+
+  @Query("SELECT COUNT(e) FROM QuestionsKr e WHERE e.deletedAt IS NULL")
+  long countByDeletedAtIsNull();
 
 }
--- src/main/java/project/maru/presentation/QuestionsKrController.java
+++ src/main/java/project/maru/presentation/QuestionsKrController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import project.maru.application.dto.questionKrDto.GetQuestionCountResponse;
 import project.maru.application.dto.questionKrDto.QuestionsKrReadResponse;
 import project.maru.application.service.QuestionsKrService;
 
@@ -20,4 +21,10 @@ public class QuestionsKrController {
       @RequestParam int n) {
     return questionsKrService.getQuestionsKrService(contentTypeId, n);
   }
+
+  @GetMapping("/questions/total-count")
+  public GetQuestionCountResponse getQuestionTotalCount() {
+    return questionsKrService.getQuestionTotalCount();
+  }
+
 }
