16bd54c0b6ed0771cdb65eae69db4f18aa4896be
bm150 <jbm20dev@gmail.com>
2024-07-16 11:27:40 +0900

    fix : 기존 swagger 요청경로 오류 수정

--- src/main/java/project/maru/presentation/middleware/SwaggerConfig.java
+++ src/main/java/project/maru/presentation/middleware/SwaggerConfig.java
@@ -6,15 +6,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @Configuration
 public class SwaggerConfig {
+
+  @Value("${cors-allow-domain.alb-end}")
+  private String ALB_DOMAIN;
+
   @Bean
   public OpenAPI openAPI() {
     String jwt = "JWT";
@@ -25,15 +29,16 @@ public class SwaggerConfig {
         .scheme("bearer")
         .bearerFormat("JWT")
     );
-
-    //Server server = new Server().url("https://kmaru.shop");
+    
+    Server server = new Server().url(ALB_DOMAIN);
     return new OpenAPI()
         .components(new Components())
         .info(apiInfo())
         .addSecurityItem(securityRequirement)
-        //.addServersItem(server)
+        .addServersItem(server)
         .components(components);
   }
+
   private io.swagger.v3.oas.models.info.Info apiInfo() {
     return new io.swagger.v3.oas.models.info.Info()
         .title("API Test") // API의 제목
