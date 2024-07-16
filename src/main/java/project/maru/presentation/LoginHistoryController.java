9596e43c60f1b3586ed2963fe885d2e5337f5242
tjrqh <asdww6631@gmail.com>
2024-07-17 06:32:35 +0900

    #81 fix : 유저 로그인 로그 로그인할 때 마다 만들어지지 않는 것 수정 및 일일 로그인 사용자 중복 카운팅 제외되는 쿼리로 수정

--- src/main/java/project/maru/presentation/LoginHistoryController.java
+++ src/main/java/project/maru/presentation/LoginHistoryController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import project.maru.application.dto.SimpleApiResponse;
 import project.maru.application.dto.loginHistoryDto.GetLoginHistoryLoginCountRequest;
 import project.maru.application.dto.loginHistoryDto.GetLoginHistoryLoginCountResponse;
 import project.maru.application.dto.loginHistoryDto.GetLoginHistoryRequest;
